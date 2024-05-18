a4fc2af71a3fe5f91c69c71d6097a53c67756a27
Koh-Du-Beom <beanleaf3260@gmail.com>
2024-05-18 18:28:04 +0900

    webChatting making

--- src/components/RoomMeeting/RoomMeeting.tsx
+++ src/components/RoomMeeting/RoomMeeting.tsx
@@ -1,36 +1,30 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store'; 
+import { RootState } from '../../store/store';
 import classes from './RoomMeeting.module.css';
 import UtilBar from './UtilBar/UtilBar';
 import ParticipantsBox from './ParticipantsBox/ParticipantsBox';
 import Chatting from './WebSocketChatting/Chatting';
 
 const RoomMeeting: React.FC = () => {
-  
   const connectionInfo = useSelector((state: RootState) => state.connectionInfo);
-
-  const [visibleChat, setVisibleChat] = useState<boolean>();
-
+  const [visibleChat, setVisibleChat] = useState<boolean>(false);
 
   return (
-    <div className={classes.container}>
-			<div className={classes.callBox}>
-        <ParticipantsBox/>
-        <UtilBar/>
-      </div>
-      <div className={classes.chatting}>
+    <div className={`${classes.container} ${visibleChat ? classes.containerSpaceBetween : classes.containerCentered}`}>
+      <div className={`${classes.callBox} ${!visibleChat ? classes.callBoxCentered : ''}`}>
+        <ParticipantsBox />
+        <UtilBar />
         <button
           className={classes.chatting_visibleButton}
-          onClick={()=>setVisibleChat(!visibleChat)}
+          onClick={() => setVisibleChat(!visibleChat)}
         >
           {visibleChat ? '<<' : '>>'}
         </button>
-        {visibleChat && <Chatting/>}
-        
       </div>
-      
-      
+      <div className={`${classes.chatting} ${visibleChat ? classes.chatting_visible : classes.chatting_hidden}`}>
+        <Chatting />
+      </div>
     </div>
   );
 };
