031ea9347f1aba02f0d1cdb2b75c180d51537e83
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-06-01 19:55:28 +0900

    7:55 룸미팅 컴포넌트 수정

--- src/components/RoomMeeting/RoomMeeting.tsx
+++ src/components/RoomMeeting/RoomMeeting.tsx
@@ -190,23 +190,23 @@ const RoomMeeting: React.FC = () => {
             </div>
 
             {mainStreamManager !== undefined ? (
-                <div id="main-video" className="col-md-6">
-                    <UserVideoComponent streamManager={mainStreamManager} />
-                </div>
-            ) : null}
-            <div id="video-container" className="col-md-6">
-                {publisher !== undefined ? (
-                    <div className={`${styles.streamContainer} col-md-6 col-xs-6`} onClick={() => handleMainVideoStream(publisher)}>
-                        <UserVideoComponent streamManager={publisher} />
-                    </div>
-                ) : null}
-                {subscribers.map((sub, i) => (
-                    <div key={sub.id} className={`${styles.streamContainer} col-md-6 col-xs-6`} onClick={() => handleMainVideoStream(sub)}>
-                        <span>{sub.id}</span>
-                        <UserVideoComponent streamManager={sub} />
-                    </div>
-                ))}
-            </div>
+							<div id="main-video" className="col-md-6">
+								<UserVideoComponent streamManager={mainStreamManager} />
+							</div>
+						) : null}
+						<div id="video-container" className="col-md-6">
+								{publisher !== undefined && mainStreamManager !== publisher ? (
+									<div className={`${styles.streamContainer} col-md-6 col-xs-6`} onClick={() => handleMainVideoStream(publisher)}>
+										<UserVideoComponent streamManager={publisher} />
+									</div>
+								) : null}
+								{subscribers.map((sub, i) => (
+									<div key={sub.id} className={`${styles.streamContainer} col-md-6 col-xs-6`} onClick={() => handleMainVideoStream(sub)}>
+										<span>{sub.id}</span>
+										<UserVideoComponent streamManager={sub} />
+									</div>
+								))}
+						</div>
         </div>
     );
 }
